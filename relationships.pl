efb678f154300b33d38601236c993bddddbd114f
jazjimenez <jaztin_jacob_jimenez@dlsu.edu.ph>
2024-11-18 18:03:11 +0800

    Self sibling contradiction

--- relationships.pl
+++ relationships.pl
@@ -54,7 +54,7 @@ contradiction(self_child) :- child(X,X).                                        
 contradiction(self_parent) :- parent(X, X).                                         % Self-parent contradiction
 contradiction(circular_cousin) :- cousin(X, X).                                     % Self-cousin contradiction
 contradiction(self_grandparent) :- grandparent(X, X).                               % Self-grandparent contradiction
-
+contradiction(self_sibling) :- sibling(X, X).                                       % Self-sibling contradiction
 
 contradiction(gender_conflict) :- male(X), female(X).                               % Gender contradiction
 
